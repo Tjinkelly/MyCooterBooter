f9802e71c615071cf76617448b539eef4be8988e
Tjinkelly <tjinkelly17@yahoo.com>
2026-02-11 10:56:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // ============================================
-// 💝 MY JOSHY 💝
+// 💝 MY ONION BOY 💝
 // ============================================
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "MY HANDSOME",
+    valentineName: "JOSHY",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
